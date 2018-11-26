b6001a17ae8cb0a12d757fc0d1be226d5428eeda
JackC <jackc0022@gmail.com>
2018-11-25 22:40:54 -0800

    Made lv 2. Added drunk effects.

---
+++ DrunkDrivingTest/Assets/Scripts/DrunkCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkCamera : MonoBehaviour {
+
+    private float xorigin, yorigin;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        xorigin = player.transform.position.x - 1;
+        yorigin = player.transform.position.y - 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        xorigin = player.transform.position.x - 1;
+        yorigin = player.transform.position.y - 0.5f;
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 2f) + xorigin, Mathf.PingPong(Time.time, 1.4f) + yorigin, -10);
+    }
+
+}
--- DrunkDrivingTest/Assets/Scripts/DrunkCarController.cs
+++ DrunkDrivingTest/Assets/Scripts/DrunkCarController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour {
+public class DrunkCarController : MonoBehaviour {
 
     Rigidbody2D rb_car;
     private float turnSpeed;
@@ -19,18 +19,18 @@ public class CarController : MonoBehaviour {
     // Use this for initialization
     void Start() {
         rb_car = GetComponent<Rigidbody2D>();
-        turnSpeed = .25f;
+        turnSpeed = 1f;
         _Velocity = 0.0f; //Current speed
-        _MaxVelocity = 4f; //Max speed
-        _MaxReverse = -1.5f; //Max reverse speed
+        _MaxVelocity = 8f; //Max speed
+        _MaxReverse = -3f; //Max reverse speed
 
         GetComponent<Rigidbody2D>().drag = 1;
-        GetComponent<Rigidbody2D>().mass = 5;
-        GetComponent<Rigidbody2D>().angularDrag = 1000;
-}
+        GetComponent<Rigidbody2D>().mass = 3;
+        GetComponent<Rigidbody2D>().angularDrag = 1;
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
         Debug.Log("In Update");
         mag = rb_car.velocity.magnitude;
         animator.SetFloat("Speed", mag);    //Allows animate when moving
@@ -38,19 +38,18 @@ public class CarController : MonoBehaviour {
         _Velocity = Input.GetAxis("Vertical") * _MaxVelocity; //Forward & Backward
 
         if (Input.GetKey(KeyCode.A)) {    //Turn left
-            transform.Rotate(Vector3.forward, turnSpeed * _Velocity);
+            transform.Rotate(Vector3.forward, turnSpeed * _Velocity / 5);
             rb_car.velocity = transform.up * mag * direction(); //Prevents drifting
         }
         if (Input.GetKey(KeyCode.D)) {   //Turn right
-            transform.Rotate(Vector3.forward, -turnSpeed * _Velocity);
-            rb_car.velocity = transform.up * mag * direction(); //Prevents drifting
-    }
+            transform.Rotate(Vector3.forward, -turnSpeed * _Velocity / 5);
+        }
         if (Input.GetKeyDown(KeyCode.Space)) { //Brakes
             _MaxVelocity = 0;
-            GetComponent<Rigidbody2D>().drag = 5;
+            GetComponent<Rigidbody2D>().drag = 1.25f;
         }
         if (Input.GetKeyUp(KeyCode.Space)) {
-            _MaxVelocity = 4f;
+            _MaxVelocity = 8f;
             GetComponent<Rigidbody2D>().drag = 1;
         }
 
